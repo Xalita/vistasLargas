90cafb38a2a72ce3221a01a4f47c275cb5f32b97
Xalita <brunofneferreira@gmail.com>
2021-05-13 17:58:07 +0100

    tentativa de arranjar o click dos serviços

--- src/app/novos-servicos/novos-servicos.component.ts
+++ src/app/novos-servicos/novos-servicos.component.ts
@@ -16,61 +16,23 @@ export class NovosServicosComponent implements OnInit {
 
   categorias:any = cat;
 
-  // isVinilOn = false;
-  // isEspelhadoOn = false;
-  // isDispensadorOn=false;
-  // isLaserOn=false;
-  // isAcrilicoOn=false;
-  // isDecoracaoOn=false;
+  isSameServico= false;
 
-  
-
-
-  // getServico(value:any) {   
-
-  //   if (value === 'vinil') {
-  //     this.isVinilOn = !this.isVinilOn;
-  //     console.log(`Isto é do vinil ${this.isVinilOn}`);
-  //   } else if (value === 'espelhado') {
-  //     this.isEspelhadoOn = !this.isEspelhadoOn;
-  //     console.log(`Isto é do espelhado ${this.isEspelhadoOn}`);
-  //   } else if (value === 'dispensador') {
-  //     this.isDispensadorOn = !this.isDispensadorOn;
-  //   } else if (value === 'laser') {
-  //     this.isLaserOn = !this.isLaserOn;
-  //   } else if(value === 'acrilicos') {
-  //     this.isAcrilicoOn = ! this.isAcrilicoOn;
-  //   } else if (value === 'decoracao') {
-  //     this.isDecoracaoOn=!this.isDecoracaoOn;
-  //   }
-    
-    // for(let i = 0; i<this.categorias.length; i++){
-    // if(value[i].nome == 'vinil') {         
-    //      value[i].isTrue = !value[i].isTrue; 
-    //      console.log (`Isto é do Vinil ${value[i].isTrue}` );
-    //      this.isVinilOn = !this.isVinilOn;  
-    //    }          
-    // } 
-    // else if (value == 'espelhado') {
-    //   this.isEspelhadoOn = !this.isEspelhadoOn;
-    //   console.log (`O Espelhado é ${this.isEspelhadoOn}`)
-    //   this.isVinilOn = false;
-    // }
-      // }
-      // if (value[i].isTrue === false) {
-      //   value[i].isTrue = true;
-      //   console.log (value[i].isTrue);
-      // } else {
-      //   value[i].isTrue = false;
-      // }
-    // }
-
-  servicoAtMom=""
+  servicoAtMom="";
 
   getServico(e:any) {  
     this.servicoAtMom = e.innerHTML;
     console.log (e.innerHTML);
     console.log (this.servicoAtMom);
+  }
+
+  checkServicos(value:any){
+    if (value === this.servicoAtMom) {
+      this.isSameServico = true;
+    }
+    else {
+      this.isSameServico = false;
+    }
   }
 
   constructor() { }
