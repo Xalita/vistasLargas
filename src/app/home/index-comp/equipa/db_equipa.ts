789d5e863aa912259f387a7154aeabe31bc6dcce
xalita <brunofneferreira@gmail.com>
2021-06-23 18:04:24 +0100

    equipa feita

--- src/app/home/index-comp/equipa/db_equipa.ts
+++ src/app/home/index-comp/equipa/db_equipa.ts
@@ -6,28 +6,28 @@ export const TEAM: equipa[] = [
             id: 0,
             nome: "Nuno Bastos",
             cargo: "Big Boss",
-            imgPath: "../../assets/equipa/hombre.png",
+            imgPath: '../../../../assets/equipa/nuno.png',
             description:"O ervilhas fez isto"
           },
           {
-            id:1,
+            id: 1,
             nome: "Paula Moreira",
             cargo: "Big Boss",
-            imgPath: "../../assets/equipa/girl.png",
+            imgPath: '../../../../assets/equipa/paula.png',
             description:"O ervilhas fez isto"
           },
           {
             id:2,
             nome: "Joel Pires",
             cargo: "Designer",
-            imgPath: "../../assets/equipa/hombre.png",
+            imgPath: '../../../../assets/equipa/Joel.png',
             description:"O ervilhas fez isto"
           },
           {
             id:3,
             nome: "Paulo Moreira",
             cargo: "Produção",
-            imgPath: "../../assets/equipa/hombre.png",
+            imgPath: '../../../../assets/equipa/paulo4.png',
             description:"O ervilhas fez isto"
           },
           // {
